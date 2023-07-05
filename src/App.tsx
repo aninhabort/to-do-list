b3b58303d2f94fb7bd2be8a22d73f22395521c89
Ana Carolina Bortolini de Magalhães <carol2015bortolini@gmail.com>
2023-07-05 20:52:07 +0000

    corrigindo o css e colocando position relative no background preto

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Body from "./components/Body"
 import Header from "./components/Header"
 
 import './global.css'
+import style from './App.module.css'
 
 function App() {
   return (
-    <div>
+    <div className={style['main-page']}>
+      <div className={style['header-background']}></div>
       <Header />
       <Body />
     </div>
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,23 +2,23 @@ import styles from './Body.module.css';
 
 const Body = () => {
     return (
-        <div>
+        <div className={styles.content}>
             <div className={styles.createTask}>
                 <input className={styles.input} placeholder="Adicione uma nova tarefa" />
                 <button className={styles.buttonCreate}>
-                    Criar +
+                    Criar
                 </button>
             </div>
 
-            <div>
-                <p>Tarefas criadas <span>0</span></p>
-                <p>Concluidas <span>0</span></p>
-            </div>
-
-            <div>
+            <div className={styles.tasks}>
+                <div>
+                    <p>Tarefas criadas <span>0</span></p>
+                    <p>Concluidas <span>0</span></p>
+                </div>
                 <p>Você ainda não tem tarefas cadastradas</p>
                 <p>Crie tarefas e organize seus itens a fazer</p>
             </div>
+
         </div>
     )
 }
