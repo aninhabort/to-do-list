f3df5d2a3b6df1336fd1efd6f231597f35048306
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2023-07-13 15:01:36 -0300

    Adicionando checkbox customizado

--- src/components/taskComponent/Tasks.tsx
+++ src/components/taskComponent/Tasks.tsx
@@ -1,25 +1,49 @@
-import { Trash } from "phosphor-react";
+import { CheckCircle, Circle, Trash } from "phosphor-react";
 
 import styles from "./Task.module.css";
+import { useState } from "react";
 
 interface TasksContent {
   content: string;
-  onDelete: (content: string) => void
+  onDelete: (content: string) => void;
 }
 
 const Tasks = ({ content, onDelete }: TasksContent) => {
+  const [checked, setChecked] = useState(false);
+
   function handleDeleteTask() {
     onDelete(content);
   }
 
+  const handleCheckboxChange = () => {
+    return !checked ? setChecked(true) : setChecked(false);
+  };
+
   return (
     <div className={styles.taskContent}>
       <div className={styles.checkbox}>
-        <input type="checkbox" id="taskInput" className={styles.checkboxInput} />
-        <label className={styles.checkboxLabel} htmlFor="taskInput">{content}</label>
+        <input
+          type="checkbox"
+          id="taskInput"
+          className={styles.checkboxInput}
+          checked={checked}
+          onChange={handleCheckboxChange}
+        />
+        <label className={styles.checkboxLabel} htmlFor="taskInput">
+        {!checked ? (
+          <Circle className={styles.circle} size={20} color="var(--pink)" />
+        ) : (
+          <CheckCircle
+            className={styles.checkCircle}
+            size={20}
+            color="var(--purple)"
+          />
+        )}
+          {content}
+        </label>
       </div>
       <button onClick={handleDeleteTask} className={styles.removeButton}>
-        <Trash size={20} color="var(--gray-300)"/>
+        <Trash size={20} color="var(--gray-300)" />
       </button>
     </div>
   );
